b23cb0b35582f141b52cec13c91815572bad6690
maneeshjohn <maneeshjohno@gmail.com>
2024-10-11 21:49:55 +0530

    added file I/O and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs, io::Result};
+
+fn get_errors(text: &str) -> Vec<&str> {
+    let lines = text.split("\n");
+    let mut result = vec![];
+
+    for line in lines {
+        if line.starts_with("ERROR") {
+            result.push(line);
+        }
+    }
+    return result;
+}
+
+fn main() -> Result<()> {
+    let content = fs::read_to_string("logs.txt")?;
+    let errors = get_errors(content.as_str());
+    fs::write("out.txt", errors.join("\n"))?;
+    Ok(())
 }
